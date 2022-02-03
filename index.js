cbf858b471c1662847f2267ed812f938e1519cd5
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-03 12:43:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ const client = new Discord.Client({
 const token = process.env.TOKEN;
 const testtoken = process.env.TEST_TOKEN;
 
+
+
+client.on('ready', () => {
+    const stringlength = 69;
+    console.log("\n")
+    console.log(`     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`.bold.brightGreen)
+    console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
+    console.log(`     ┃ `.bold.brightGreen + `Discord Bot is online!`.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length-`Discord Bot is online!`.length)+ "┃".bold.brightGreen)
+    console.log(`     ┃ `.bold.brightGreen + ` /--/ ${client.user.tag} /--/ `.bold.brightGreen+ " ".repeat(-1+stringlength-` ┃ `.length-` /--/ ${client.user.tag} /--/ `.length)+ "┃".bold.brightGreen)
+    console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
+    console.log(`     ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`.bold.brightGreen)
+});
+
+
+
+
 client.on("messageCreate", message => {
     if (message.content === "test-") {
     const row = new MessageActionRow()
