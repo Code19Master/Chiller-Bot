0a8e9785836696c903e7c9d59f31214cf01b1679
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-03 13:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,34 +59,8 @@ client.on('ready', () => {
 
 client.on("messageCreate", message => {
     if (message.content === "test-") {
-    const row = new MessageActionRow()
-    .addComponents(
-        new MessageSelectMenu()
-            .setCustomId('select')
-            .setPlaceholder('Nothing selected')
-            .setMinValues(2)
-            .setMaxValues(3)
-            .addOptions([
-                {
-                    label: 'Select me',
-                    description: 'This is a description',
-                    value: 'first_option',
-                },
-                {
-                    label: 'You can select me too',
-                    description: 'This is also a description',
-                    value: 'second_option',
-                },
-                {
-                    label: 'I am also an option',
-                    description: 'This is a description as well',
-                    value: 'third_option',
-                },
-            ]),
-    );
-    }
-
-    message.channel.send({ content: 'work', components: [row] });
+    message.channel.send("work pls");
+}
 })
 
 client.login(token);
