997b47b4e92ceaaeb0ea169bc620ef2b3e6a191e
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-04 10:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -116,30 +116,13 @@ client.on('messageCreate', async message => {
           
 			);
 
-     const btnRow = new MessageActionRow().addComponents(
-      new MessageButton()
-      .setLabel('Invite Me')
-      .setStyle('LINK')
-      .setURL('https://discord.com/api/oauth2/authorize?client_id=936617927833178132&permissions=8&scope=bot%20applications.commands'),
- 
 
-      new MessageButton()
-      .setLabel('Support Server')
-      .setStyle('LINK')
-      .setURL('https://discord.gg/59PfqAUN3Z'),
-
-      new MessageButton()
-      .setLabel('Vote Me')
-      .setStyle('LINK')
-      .setURL('https://discord.gg/59PfqAUN3Z'),//Change Link To Vot Me TOPGG 
-     )
-      
 
 
 
   
  
-  await message.channel.send({ content: 'Pong!', components: [row] [btnRow]  });
+  await message.channel.send({ content: 'Pong!', components: [row] });
 
 
     }
