64c8ca01eae8d53a46748992318453f96ce71b1d
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-11 00:15:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -358,13 +358,21 @@ if (message.content.startsWith("@suggest")) {
   }
 
 //kick command 
-if (message.content.startsWith("@kick")) {
-  const kickMessage = message.content.slice(5);
-  if(!kickMessage) return message.channel.send("Please Provide A User To Kick!");
-  message.delete();
-  message.channel.send("User Has Been Kicked!");
-  message.guild.member(message.mentions.users.first()).kick();
+if (message.content.toLowerCase().startsWith('@kick')) {
+  const member = message.mentions.members.first()
+  if (!member) return message.channel.send('You need to mention a user/provide an ID')
+  if (!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send('You lack the required permissions')
+  if (member.permissions.has('KICK_MEMBERS') || member.permissions.has('BAN_MEMBERS')) return message.channel.send('This user seems to be a moderator')
+ 
+  try {
+  member.kick().then(() => {
+  message.channel.send(`Kicked ${member}`)
+  })
+  } catch (err) {
+  console.log(err)
+  message.channel.send('Oops, something went wrong ')
   }
+ }
 
 
   
