3a1e13d34465c7a48581564dff60ca08333cdb26
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-11 00:17:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -362,7 +362,6 @@ if (message.content.toLowerCase().startsWith('@kick')) {
   const member = message.mentions.members.first()
   if (!member) return message.channel.send('You need to mention a user/provide an ID')
   if (!message.member.permissions.has('KICK_MEMBERS')) return message.channel.send('You lack the required permissions')
-  if (member.permissions.has('KICK_MEMBERS') || member.permissions.has('BAN_MEMBERS')) return message.channel.send('This user seems to be a moderator')
  
   try {
   member.kick().then(() => {
