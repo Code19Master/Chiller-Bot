f53f69a1ae0dc9dfe7560107dc5d7815f3b4a1e7
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-11 23:09:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -386,31 +386,12 @@ simplydjs.calculator(message, {
 //nqn
 simplydjs.nqn(message);
 
-//whois 
-  if(message.content === `@whois`) {
-    let user;
-    if (message.mentions.users.first()) {
-        user = message.mentions.users.first();
-    } 
-    
-    const member = message.guild.member(user);
-    
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setThumbnail(message.author.avatarURL)
-        .addField(`${user.tag}`, `${user}`, true)
-        .addField("ID:", `${user.id}`, true)
-        .addField("Nickname:", `${member.nickname !== null ? `${member.nickname}` : 'None'}`, true)
-        .addField("Status:", `${user.presence.status}`, true)
-        .addField("In Server", message.guild.name, true)
-        .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
-        .addField("Bot:", `${user.bot}`, true)
-        .addField("Joined The Server On:", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
-        .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-        .addField("Roles:", member.roles.map(roles => `${roles}`).join(', '), true)
-        .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
-    message.channel.send({embeds: [embed]});
-   }
+//simprate command
+if (message.content.toLowerCase().startsWith('@simprate')) {
+    let user = message.mentions.users.first() || message.author
+    let simps = Math.floor(Math.random() * 100) + 1;
+    message.channel.send(`${user} is **${simps}**% Simp. SIMP!`)
+}
 
   
 
