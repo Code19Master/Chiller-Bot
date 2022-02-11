2baad098b7b18f0c8a5c778c78cafe6a6bdb3205
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-11 23:53:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -416,7 +416,7 @@ if(message.content.startsWith("@8ball")) {
   .setTitle("8ball's Answer")
   .setDescription(`:8ball: 8ball's Answer: ${replies[Math.floor(Math.random() * replies.length)]}`)
   .setColor("RED")
-  .setFooter("8ball has given you an answer")
+  .setFooter("Question - " + args)
   message.channel.send({ embeds: [embed] })
   }
 
