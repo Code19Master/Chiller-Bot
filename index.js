2d0fc48eb185417cf4333f2f9209d95c5fc782b2
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-12 18:00:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MessageButton } = require('discord.js');
 const { Client, Intents, Collection } = require('discord.js');
 const akinator = require("discord.js-akinator")
 const simplydjs = require("simply-djs");
+const random = require("something-random-on-discord").Random
 const client = new Discord.Client({
     intents: [ Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MEMBERS,
@@ -583,58 +584,10 @@ if(message.content.startsWith("@8ball")) {
     }
 
 //memes
-
- if (message.content.startsWith('@meme')) {
- https.get(url, result => {
- var body = '';
- result.on('data', chunk => {
- body += chunk;
- });
-
- result
- .on('end', () => {
- var response = JSON.parse(body);
- var index =
- response.data.children[Math.floor(Math.random() * 99) + 1].data;
-
- var link = 'https://reddit.com' + index.permalink;
-
- if (index.post_hint !== 'image') {
- var text = index.selftext;
- const textembed = new Discord.MessageEmbed()
- .setTitle(`${title}`)
- .setColor('RANDOM')
- .setURL(link);
-
- message.channel.send({ embeds: [textembed]});
- }
-
- var image = index.preview.images[0].source.url.replace('&amp;', '&');
- var title = index.title;
- var subRedditName = index.subreddit_name_prefixed;
-
- if (index.post_hint !== 'image') {
- const textembed = new Discord.MessageEmbed()
- .setTitle(`${title}`)
- .setColor('RANDOM')
- .setURL(link);
-
- message.channel.send({ embeds: [textembed] });
- }
- const imageembed = new Discord.MessageEmbed()
- .setTitle(`${title}`)
- .setImage(image)
- .setColor('RANDOM')
- .setURL(link);
- message.channel.send({ embeds: imageembed });
- })
- .on('error', function(e) {
- console.log('Got an error: ', e);
- });
- });
- }
-
-
+  if(message.content.startsWith("@meme")) {
+    let data = await random.getMeme()
+    message.channel.send(data)
+  }
 
 
   
