74312eb78b4a6e07b948169f9b88ff020e17810f
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-12 17:44:10 +0530

    Update Commands.md

--- index.js
+++ index.js
@@ -581,6 +581,62 @@ if(message.content.startsWith("@8ball")) {
 
     }
 
+//memes
+  if(message.content.startsWith("@meme")) {
+    const https = require('https');
+ const url = 'https://www.reddit.com/r/memes/hot/.json?limit=100';
+
+ if (message.content.startsWith('d!meme')) {
+ https.get(url, result => {
+ var body = '';
+ result.on('data', chunk => {
+ body += chunk;
+ });
+
+ result
+ .on('end', () => {
+ var response = JSON.parse(body);
+ var index =
+ response.data.children[Math.floor(Math.random() * 99) + 1].data;
+
+ var link = 'https://reddit.com' + index.permalink;
+
+ if (index.post_hint !== 'image') {
+ var text = index.selftext;
+ const textembed = new Discord.MessageEmbed()
+ .setTitle(`${title}`)
+ .setColor('RANDOM')
+ .setURL(link);
+
+ message.channel.send({ embeds: [textembed]});
+ }
+
+ var image = index.preview.images[0].source.url.replace('&amp;', '&');
+ var title = index.title;
+ var subRedditName = index.subreddit_name_prefixed;
+
+ if (index.post_hint !== 'image') {
+ const textembed = new Discord.MessageEmbed()
+ .setTitle(`${title}`)
+ .setColor('RANDOM')
+ .setURL(link);
+
+ message.channel.send({ embeds: [textembed] });
+ }
+ const imageembed = new Discord.MessageEmbed()
+ .setTitle(`${title}`)
+ .setImage(image)
+ .setColor('RANDOM')
+ .setURL(link);
+ message.channel.send({ embeds: imageembed });
+ })
+ .on('error', function(e) {
+ console.log('Got an error: ', e);
+ });
+ });
+ }
+  }
+
 
 
 
