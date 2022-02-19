90ab4d8c80a2e5372aea69741c5937d8ac02ece2
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-19 14:28:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -778,6 +778,36 @@ if(!sniped) {
 }
 }
 
+//kill
+if(message.content.startsWith("#kill")) {
+  let victim = message.mentions.users.first()
+  if(!victim) message.reply("Mention someone to Kill")
+  else{
+ let replies = [ (`${victim} has been shot`), (`${victim} has been stabbed`), (`${victim} has been drowned`), 
+  (`${victim} has been electrified`), (`A goose honked at ${victim} to death`), 
+  (`Some psychopath zapped ${victim} with his laser eyes`), 
+  (`${victim} ate a poisonous potato`), (`${victim} died from slowmode being to long`), 
+  (`${victim} was run over by car`), (`${victim} was pushed in lava`), (`${victim} was banned by the server owner`), 
+  (`${victim} was found dead in a dumpster`), 
+  (`Someone named Joe was found chewing on ${victim}'s leg`), (`${victim} got drunk and fell in the water`), 
+  (`${victim} made a deal with the devil`), (`${victim} was hacked by an 
+ Oreo`), (`An alien named MEE6 abducted ${victim} in their sleep`),]
+ 
+  message.channel.send(`${replies[Math.floor(Math.random() * replies.length)]}`) 
+  }
+  }
+
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    if(interaction.commandName == "help"){
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Help Commands of, bot-name")
+    .addField("/help","THIS")
+    .setColor("RANDOM")
+    .setTimestamp()
+        interaction.reply({embeds:[embed]})
+    }})
+
 
 
 
@@ -789,16 +819,7 @@ if(!sniped) {
 
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-  if(interaction.commandName == "help"){
-  let embed = new Discord.MessageEmbed()
-  .setTitle("Help Commands of, bot-name")
-  .addField("/help","THIS")
-  .setColor("RANDOM")
-  .setTimestamp()
-      interaction.reply({embeds:[embed]})
-  }})
+
 
 
 
