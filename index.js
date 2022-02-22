ef48edc6725a28d22a7d9c1cc75daaa3981afcda
CodeMaster-file <70687571+CodeMaster-file@users.noreply.github.com>
2022-02-22 23:56:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1255,10 +1255,10 @@ message.reply({embeds: [embedError]})
   
 //terminal
 if(message.content.startsWith(prefix + "terminal")) {
-  let args = message.content.slice(10).split(" ");
+  let args = message.content.slice(10)
   if (!args) return message.channel.send("No input");
   require("child_process").exec(args, (err, stdout, stderr) =>{
-if (err) return "f";
+if (err) return "";
 message.channel.send(stdout)
 })
 }
