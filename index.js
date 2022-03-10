bd0814d931e5b95d8be3c33db6ec086217fc6668
CodeMaster100 <divikbabbar@gmail.com>
2022-03-10 23:49:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ const gameType = "character";
 const useButtons = true; 
 const embedColor = "#000000";
 const esnipes = {};
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
 discordModals(client); // discord-modals needs your client in order to interact with modals 
 
@@ -1763,6 +1764,38 @@ const Embed = new MessageEmbed()
 return message.channel.send({embeds: [Embed]});
 
 }
+
+//github
+if (message.content.startsWith(prefix + "github")) {
+  if (!args[0]) return message.channel.send(`Please Give Me A Username!`)
+    
+  fetch(`https://api.github.com/users/${args.join('-')}`)
+    .then(res => res.json()).then(body => {
+      if(body.message) return message.channel.send(`User Not Found | Please Give Me A Valid Username!`);
+    let { login, avatar_url, name, id, html_url, public_repos, followers, following, location, created_at, bio } = body;
+
+            const embed = new MessageEmbed()
+            .setAuthor(`${login} Information!`, avatar_url)
+            .setColor(`#211F1F`)
+            .setThumbnail(`${avatar_url}`)
+            .addField(`Username`, `${login}`)
+            .addField(`ID`, `${id}`)
+            .addField(`Bio`, `${bio || "No Bio"}`)
+            .addField(`Public Repositories`, `${public_repos || "None"}`, true)
+            .addField(`Followers`, `${followers}`, true)
+            .addField(`Following`, `${following}`, true)
+            .addField(`Location`, `${location || "No Location"}`)
+            .addField(`Account Created`, moment.utc(created_at).format("dddd, MMMM, Do YYYY"))
+            .setFooter(`Tysm For Using Me! ${message.author.username}`)
+
+            message.channel.send(embed)
+
+    })
+
+         .catch(console.error);
+          return message.channel.send(`Something Went Wrong Try Again Later!`)
+        }
+
 
 
 
