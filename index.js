e430f50496ec5ef5f1e057f01c344e84e996f33a
CodeMaster100 <divikbabbar@gmail.com>
2022-03-11 23:47:06 +0530

    Random urban Command

--- index.js
+++ index.js
@@ -1828,6 +1828,34 @@ if (message.content.startsWith(prefix + "urban")) {
 }
 
 
+//random urban
+if (message.content.startsWith(prefix + "rurban")) {
+  
+  let image = "http://cdn.marketplaceimages.windowsphone.com/v8/images/5c942bfe-6c90-45b0-8cd7-1f2129c6e319?imageType=ws_icon_medium";
+  try {
+      let res = await randomurban()
+          if (!res) return message.channel.send("No results found for this topic, sorry!");
+          let { word, urbanURL, definition, example, thumbsUp, thumbsDown, author } = res;
+
+          let embed = new MessageEmbed()
+              .setColor("GREEN")
+              .setAuthor(`Urban - ${word}`)
+              .setThumbnail(image)
+              .setDescription(`**Meaning:**\n*${definition || "No meaning"}*\n\n**Example:**\n*${example || "No Example"}*`)
+              .addField('**Rating:**', `**\`Upvotes: ${thumbsUp} | Downvotes: ${thumbsDown}\`**`)
+              .addField("**Link**",  `[link to ${word}](${urbanURL})`)
+              .addField("**Author:**", `${author || "unknown"}`)
+              .setTimestamp()
+
+          message.channel.send({embeds: [embed]})
+      
+  } catch (e) {
+      console.log(e)
+      return message.channel.send("looks like i've broken! Try again")
+  }
+}
+
+
 
 
 
